1085a0f701a8d5cb6b76e5d323110366899330e8
abdnafees <abdullahnafees23@gmail.com>
2024-07-21 00:52:16 +0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -144,6 +144,11 @@ class Scanner:
             return '\0'
         return self.source[self.current]
 
+    def peek_next(self):
+        if self.current + 1 >= len(self.source):
+            return '\0'
+        return self.source[self.current + 1]
+
     def add_token(self, type, literal=None):
         text = self.source[self.start:self.current]
         self.tokens.append(Token(type, text, literal, self.line))
